35e8079721bc52e19bf8f4b61b54ea5ddc36a361
Nafisa06 <nafisa.ila@gmail.com>
2022-02-07 01:05:34 +1100

    entered array for readme questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,57 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "ProjectTitle",
+        message: "Type in the README Title: ",
+    },
+    {
+        type: "input",
+        name: "TableOfContents",
+        message: "Type in README Table of Contents: ",
+    },
+    {
+        type: "input",
+        name: "Projectdescription",
+        message: "Describe the project: ",
+    },
+    {
+        type: "input",
+        name: "installionInstructions",
+        message: "Advise the commands needed to run the installion of the README generators: ",
+    },
+    {
+        type: "input",
+        name: "usageInstructions",
+        message: "Describe how to use the README generator: ",
+    },
+    {
+        type: "input",
+        name: "contributionGuide",
+        message: "Name any contributors to the project: ",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Commands that should be run to run tests README generator: ",
+    },
+    {
+        type: "input",
+        name: "githubUsername",
+        message: "Add in your Github User Name: ",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Add in README License: ",
+        choices: [
+            "MIT"
+        ]
+    } 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
